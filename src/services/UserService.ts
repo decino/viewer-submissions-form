0416cebfe811552561e5ebbece2f812fed9da655
VictoriqueMoe <loli@victorique.moe>
2022-12-21 00:55:51 +0000

    swap to sha256

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,7 +2,7 @@ import {Inject, Service} from "@tsed/di";
 import {SQLITE_DATA_SOURCE} from "../model/di/tokens";
 import {DataSource} from "typeorm";
 import {UserModel} from "../model/db/User.model";
-import * as argon2 from "argon2";
+import sha256 from "sha256";
 
 @Service()
 export class UsersService {
@@ -11,13 +11,14 @@ export class UsersService {
     private ds: DataSource;
 
     public async getUser(email: string, password: string): Promise<UserModel | null> {
+        const hash = sha256(password);
         const userObject = await this.ds.manager.findOne(UserModel, {
             where: {
-                email
+                email,
+                password: hash
             }
         });
-        // use safe timings compare to verify the hash matches
-        if (!userObject || !await argon2.verify(userObject.password, password)) {
+        if (!userObject) {
             return null;
         }
         return userObject;

9a161707a14e7f5e34b8830d8fbfeb665a052748
VictoriqueMoe <loli@victorique.moe>
2022-12-24 01:05:28 +0000

    Revert "swap to sha256"
    This reverts commit 0416cebfe811552561e5ebbece2f812fed9da655.

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,7 +2,7 @@ import {Inject, Service} from "@tsed/di";
 import {SQLITE_DATA_SOURCE} from "../model/di/tokens";
 import {DataSource} from "typeorm";
 import {UserModel} from "../model/db/User.model";
-import sha256 from "sha256";
+import * as argon2 from "argon2";
 
 @Service()
 export class UsersService {
@@ -11,14 +11,13 @@ export class UsersService {
     private ds: DataSource;
 
     public async getUser(email: string, password: string): Promise<UserModel | null> {
-        const hash = sha256(password);
         const userObject = await this.ds.manager.findOne(UserModel, {
             where: {
-                email,
-                password: hash
+                email
             }
         });
-        if (!userObject) {
+        // use safe timings compare to verify the hash matches
+        if (!userObject || !await argon2.verify(userObject.password, password)) {
             return null;
         }
         return userObject;

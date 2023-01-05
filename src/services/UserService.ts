d4c0209ffcb0a9b8f2d827ec66e8510136f02148
VictoriqueMoe <loli@victorique.moe>
2023-01-05 11:45:56 +0000

    bump versions

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,7 +2,7 @@ import {Inject, Service} from "@tsed/di";
 import {SQLITE_DATA_SOURCE} from "../model/di/tokens";
 import {DataSource} from "typeorm";
 import {UserModel} from "../model/db/User.model";
-import * as argon2 from "argon2";
+import argon2 from "argon2";
 
 @Service()
 export class UsersService {

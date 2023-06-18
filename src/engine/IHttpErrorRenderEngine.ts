8f003b302eaac9fab47065b2b5eac8f0cedc34bf
VictoriqueMoe <loli@victorique.moe>
2023-06-18 18:15:23 +0100

    added doc

--- src/engine/IHttpErrorRenderEngine.ts
+++ src/engine/IHttpErrorRenderEngine.ts
@@ -3,7 +3,16 @@ import {Exception} from "@tsed/exceptions";
 import {PlatformResponse} from "@tsed/common";
 
 export interface IHttpErrorRenderEngine<T> {
+    /**
+     * Get the render template for this exception engine.
+     * @param obj
+     * @param response
+     */
     render(obj: HttpErrorRenderObj, response: PlatformResponse): Promise<T>;
 
+    /**
+     * Returns true if this render engine supports the exception thrown by the system
+     * @param exception
+     */
     supportsError(exception: Exception): boolean;
 }

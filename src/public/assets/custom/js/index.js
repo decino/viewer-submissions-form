d07a69c7ac7e6962b9ef3373b52b6b23ca86ae14
VictoriqueMoe <loli@victorique.moe>
2023-08-14 15:22:56 +0100

    fix issue with disabling submit

--- src/public/assets/custom/js/index.js
+++ src/public/assets/custom/js/index.js
@@ -53,15 +53,12 @@ Site.loadPage(async function (site) {
         document.getElementById("wadFile")?.addEventListener("change", async ev => {
             const file = ev.currentTarget.files[0];
             const fileSizeAlert = document.getElementById("fileSizeError");
-            const submitButton = document.getElementById("submit");
             const uploadFile = ev.target;
             if (file && file.size > fileSizeLimit) {
                 Site.display(false, fileSizeAlert);
-                submitButton.disabled = true;
                 uploadFile.value = null;
             } else {
                 Site.display(true, fileSizeAlert);
-                submitButton.disabled = false;
             }
             const ext = file.name?.split(".")?.pop()?.toLowerCase();
             const select = document.getElementById("levelToPlaySelect");

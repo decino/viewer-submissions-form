13e600bd134000359f7b9f24bf0c01664f52ee38
Walker Aldridge <daldridge@lairdscomputer.com>
2024-12-12 14:42:49 -0600

    Copy enchanter when build finishes

--- buildScripts/moveFiles.mjs
+++ buildScripts/moveFiles.mjs
@@ -4,6 +4,7 @@ import fs from "node:fs/promises";
 
 const from = `${path.dirname(fileURLToPath(import.meta.url))}/../dist/src`;
 const to = `${path.dirname(fileURLToPath(import.meta.url))}/../dist`;
+const enchanter = `${path.dirname(fileURLToPath(import.meta.url))}/../src/public/assets/vendor/Enchanter`;
 
 const moveFiles = async (sourceDir, destinationDir) => {
     try {
@@ -14,6 +15,9 @@ const moveFiles = async (sourceDir, destinationDir) => {
             return fs.rename(sourceFile, destFile);
         });
         await Promise.all(pArr);
+        const enchanterSource = path.join(enchanter, 'enchanter.js');
+        const enchanterDest = path.join(destinationDir, 'public/assets/vendor/Enchanter/enchanter.js');
+        await fs.copyFile(enchanterSource, enchanterDest);
         console.log("All files moved successfully");
     } catch (err) {
         console.error("An error occurred while moving files", err);
